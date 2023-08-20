0dcea39642d2c081b377fb2aab03c897e974538a
60032 <60032@SERGE>
2023-08-20 19:37:55 +0300

    Add project files.

---
+++ explore_static_interface_members_generic_math/Program.cs
@@ -0,0 +1,46 @@
+﻿// Note: Not complete. This won't compile yet.
+using System.Numerics;
+
+var pt = new Point<int>(3, 4);
+
+var translate = new Translation<int>(5, 10);
+
+var final = pt + translate;
+
+//Console.WriteLine("фывпвыфв");
+
+Console.WriteLine(pt);
+Console.WriteLine(translate);
+Console.WriteLine(final);
+
+public record Translation<T>(T XOffset, T YOffset) : IAdditiveIdentity<Translation<T>, Translation<T>>
+    where T : IAdditionOperators<T, T, T>, IAdditiveIdentity<T, T>
+{
+    public static Translation<T> AdditiveIdentity =>
+        new Translation<T>(XOffset: T.AdditiveIdentity, YOffset: T.AdditiveIdentity);
+}
+
+public record Point<T>(T X, T Y) : IAdditionOperators<Point<T>, Translation<T>, Point<T>>
+    where T : IAdditionOperators<T, T, T>, IAdditiveIdentity<T, T>
+{
+    public static Point<T> operator +(Point<T> left, Translation<T> right) =>
+        left with { X = left.X + right.XOffset, Y = left.Y + right.YOffset };
+}
+
+/*class Program
+{
+    static void Main(string[] args)
+    {
+        var pt = new Point<int>(3, 4);
+
+        var translate = new Translation<int>(5, 10);
+
+        var final = pt + translate;
+
+        Console.WriteLine(pt);
+        Console.WriteLine(translate);
+        Console.WriteLine(final);
+        // Display the number of command line arguments.
+        Console.WriteLine(args.Length);
+    }
+}*/
---
+++ explore_static_interface_members_repeat_sequence/Program.cs
@@ -0,0 +1,26 @@
+﻿var str = new RepeatSequence();
+
+for (int i = 0; i < 10; i++)
+    Console.WriteLine(str++);
+
+Console.WriteLine(str);
+Console.WriteLine(str);
+
+public interface IGetNext<T> where T : IGetNext<T>
+{
+    static abstract T operator ++(T other);
+}
+
+public struct RepeatSequence : IGetNext<RepeatSequence>
+{
+    private const char Ch = 'A';
+    private string Text = new string(Ch, 1);
+
+    public RepeatSequence() { }
+
+    public static RepeatSequence operator ++(RepeatSequence other)
+        => other with { Text = other.Text + Ch };
+
+    public override string ToString() => Text;
+}
+
